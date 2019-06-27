a8657aedef2c96cf5ece909e647135fe38bd220d
bjorkymeatballs <52300661+bjorkymeatballs@users.noreply.github.com>
2019-06-27 19:36:48 -0400

    Add files via upload

---
+++ js/annotations.js
@@ -0,0 +1,17 @@
+function toggle01() {
+	var x = document.getElementById("a1");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle02() {
+	var x = document.getElementById("a2");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
