1076a50b95d152df364bdd16853f704f95fe6849
bjorkymeatballs <52300661+bjorkymeatballs@users.noreply.github.com>
2019-10-12 16:34:53 -0400

    Add files via upload

--- js/annotations.js
+++ js/annotations.js
@@ -15,3 +15,174 @@ function toggle02() {
 			x.style.display = "none";
 		}
 }
+
+function toggle03() {
+	var x = document.getElementById("a3");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle04() {
+	var x = document.getElementById("a4");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle05() {
+	var x = document.getElementById("a5");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle06() {
+	var x = document.getElementById("a6");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle07() {
+	var x = document.getElementById("a7");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle08() {
+	var x = document.getElementById("a8");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle09() {
+	var x = document.getElementById("a9");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle10() {
+	var x = document.getElementById("a10");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle11() {
+	var x = document.getElementById("a11");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle12() {
+	var x = document.getElementById("a12");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle13() {
+	var x = document.getElementById("a13");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle14() {
+	var x = document.getElementById("a14");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle15() {
+	var x = document.getElementById("a15");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle16() {
+	var x = document.getElementById("a16");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle17() {
+	var x = document.getElementById("a17");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle18() {
+	var x = document.getElementById("a18");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle19() {
+	var x = document.getElementById("a19");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle20() {
+	var x = document.getElementById("a20");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
+
+function toggle21() {
+	var x = document.getElementById("a21");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+}
